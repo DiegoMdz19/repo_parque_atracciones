76766452ca28504ee6bcc817cc70c13a00db2b84
Canichedelviche2 <ikercepi@gmail.com>
2025-12-04 19:16:23 +0100

    Crud_atracciones_visitante

--- repositories/repo_atraccion.py
+++ repositories/repo_atraccion.py
@@ -1,6 +1,41 @@
 from peewee import *
 from playhouse import postgres_ext
-from models.model_atraccion import AtraccionModel
+from models.model_atraccion import Atraccion
 
 class RepoAtraccion:
-    pass
+    @staticmethod
+    def create(nombre,tipo,altura_minima,detalles=None):
+        try:
+            if detalles:
+                return Atraccion.create(nombre=nombre,tipo=tipo,altura_minima=altura_minima,detalles=detalles)
+            else:
+                return Atraccion.create(nombre=nombre,tipo=tipo,altura_minima=altura_minima)
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def search_all():
+        return list(Atraccion.select())
+    @staticmethod
+    def search_disp():
+        
+        atrac = (
+            Atraccion.select()
+            .where(Atraccion.activa == True)
+        )
+
+        return list(atrac)
+    
+    @staticmethod
+    def modificar_activa(id):
+        atrac = Atraccion.get(Atraccion.id == id)
+        if not atrac:
+            return
+        if Atraccion.activa == True:
+            Atraccion.activa = False
+        else:
+            Atraccion.activa = True
+        atrac.save()
+        return atrac
+    
+    
--- repositories/repo_visitante.py
+++ repositories/repo_visitante.py
@@ -1,7 +1,40 @@
 from peewee import *
 from playhouse import postgres_ext
-from models.model_visitante import VisitanteModel
+from models.model_visitante import Visitante
+from models.model_atraccion import Atraccion
+from models.model_ticket import Ticket
 
 
 class RepoVisitante:
-    pass
+
+    @staticmethod
+    def create(nombre,email,preferencias=None):
+        try:
+            if preferencias:
+                return Visitante.create(nombre=nombre,email=email,preferencias=preferencias)
+            else:
+                return Visitante.create(nombre=nombre,email=email)
+        except Exception as e:
+            print(e)
+            return None
+
+    @staticmethod
+    def search_all():
+        return list(Visitante.select())
+    
+    @staticmethod
+    def delete(id):
+        return Visitante.delete().where(Visitante.id == id)
+    
+    def visit_extremas():
+        json_struct = {"tipo_favorito": ["extrema"]}
+        
+        visit = (
+            Visitante.select()
+            .where(Visitante.preferencias.contains(json_struct))
+        )
+
+        return list(visit)
+    
+    def visit_tickets():
+        return list(Visitante.select().where(Ticket.visiante_id == Visitante.id))
